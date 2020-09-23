3d7e2da01c55ddcf64e668e78f9209d9659b5269
Jacob Higgins <higginsjake909@gmail.com>
2020-09-22 23:25:13 -0500

    parsing instructions

--- lab2.c
+++ lab2.c
@@ -6,7 +6,29 @@
 
 int main(void)
 {
+	printf("Assembly to Machine Code Generator \n");
 	char test1[] = "addiu $a0, $zero, 0";
+	char * pieces[5];
+	char * parsedLine;
+	int i = 0;
+	pieces[4] = NULL;
+
+	// Parsing the test string
+	parsedLine = strtok(test1, " $,");
+	while(parsedLine != NULL)
+	{
+		pieces[i] = parsedLine;
+		parsedLine = strtok(NULL, " $,");
+		i++;
+	}
+	// printing...
+	printf("Segments of instruction: \n");
+	i = 0;
+	while(pieces[i] != NULL)
+	{
+		printf("%s \n", pieces[i]);
+		i++;
+	}
 
 
 	return 0;
