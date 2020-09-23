898a88c1ab6de0b1f13347ae62468f1a86552c59
Jacob Higgins <higginsjake909@gmail.com>
2020-09-22 22:37:05 -0500

    Second commit

---
+++ lab2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <assert.h>
+
+int main(void)
+{
+	char test1[] = "addiu $a0, $zero, 0";
+
+
+	return 0;
+}
